8fd68eb7200c478b78101f6a7ed83b18b6238cef
saurabh-potdar <77050212+saurabh-potdar@users.noreply.github.com>
2021-09-26 23:03:04 +0530

    heyey

---
+++ infytel-zuul/src/test/java/com/infosys/infytel/zuul/InfytelZuulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.infosys.infytel.zuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InfytelZuulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
